9a3293c7d1a33ff6f3132c5cbaa519e274161dc8
thuynt <thuybinh11997@gmail.com>
2020-11-11 23:35:43 +0700

    fix UI

--- routes/web.php
+++ routes/web.php
@@ -27,14 +27,12 @@ Route::get('/admin', 'HomeController@index')->name('home');
 
 Route::group(['middleware' => ['auth']], function () {
     //Category
-
     Route::post('/add-category','CategoryController@add_category');
     Route::get('category','CategoryController@all_category');
     Route::get('category/{id}','CategoryController@delete_category');
     Route::get('editcategory/{id}','CategoryController@edit_category');
     Route::post('update-category/{id}','CategoryController@update_category');
     Route::get('/deletecategory/{id}','CategoryController@selected_category');
-
 //Post
     Route::get('/post','PostController@all_Post');
     Route::post('/savepost','PostController@save_post');
